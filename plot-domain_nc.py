07d75a300e13190a47fb6d7c4e10d69f3e49da20
caio ruman <cruman@tamer.meteo.mcgill.ca>
2018-12-14 16:58:52 -0500

    New changes

--- plot-domain_nc.py
+++ plot-domain_nc.py
@@ -16,72 +16,76 @@ Reads a Netcdf file and plot the domain over the globe
 
 #r = RPN('/glacier/caioruman/Data/Geophys/PanArctic0.5/pan_artic_me_0.5')
 #r = RPN('/home/cruman/scratch/CanArc_004deg_1500x900.fst')
-nc = Dataset('/home/cruman/scratch/CanArc_004deg_1500x900.nc')
+#nc = Dataset('CanArc_004deg_1500x900.nc')
 
 #var = "MSKC"
 
 #me = nc.variables[var][0,0]
-lon = nc.variables['lon'][:]
-lat = nc.variables['lat'][:]
+#lon = nc.variables['lon'][:]
+#lat = nc.variables['lat'][:]
 
 #lon, lat = r.get_longitudes_and_latitudes_for_the_last_read_rec()
 
+m = Basemap(resolution='l',projection='ortho',lon_0=-90,lat_0=70)
 m = Basemap(resolution='l',projection='ortho',lon_0=-90,lat_0=90)
 #m = Basemap(resolution='l', projection='', 
 
-fig1 = plt.figure(figsize=(7, 11), frameon=False, dpi=100)
+fig1 = plt.figure(figsize=(14, 22), frameon=False, dpi=200)
 ax = fig1.add_axes([0.1,0.1,0.8,0.8])
 
-print(lon[:,0].shape)
-print(lat[0,:].shape)
-print(lon.shape)
-print(lat.shape)
-newlon = np.where(lon <= 180, lon, lon - 360)
+#print(lon[:,0].shape)
+#print(lat[0,:].shape)
+#print(lon.shape)
+#print(lat.shape)
+#newlon = np.where(lon <= 180, lon, lon - 360)
 
 #for i in range(0,lat[0,:].shape[0],5):
 #    print(lon[:,i])
 #    print(lat[:,i])
 #    x, y = m(newlon[:,i], lat[:,i])
-     x = lon[:,i]
-     y = lat[:,i]
+#     x = lon[:,i]
+#     y = lat[:,i]
 #    print(x)
 #    print(y)
-     m.plot(x, y, color='red', linewidth=0.25)
+#     m.plot(x, y, color='red', linewidth=0.25)
 
-for i in range(0,lon[:,0].shape[0],5):
-    x, y = m(lon[i,:], lat[i,:])
-    m.plot(x, y, color='red', linewidth=0.25)
+#for i in range(0,lon[:,0].shape[0],5):
+#    x, y = m(lon[i,:], lat[i,:])
+#    m.plot(x, y, color='red', linewidth=0.25)
 
-ii = [0,-1]
+#ii = [0,-1]
 
-for i in ii:
+#for i in ii:
         
-    x,y = m(lon[:,i],lat[:,i])
-    m.plot(x,y,color='red')
+#    x,y = m(lon[:,i],lat[:,i])
+#    m.plot(x,y,color='red')
 
-    x,y = m(lon[i,:],lat[i,:])
-    m.plot(x,y,color='red')
+#    x,y = m(lon[i,:],lat[i,:])
+#    m.plot(x,y,color='red')
 
 # Blending domain
-bb = 40
-ii = [bb,-bb]
+#bb = 40
+#ii = [bb,-bb]
 
-for i in ii:
+#for i in ii:
 
-    x,y = m(lon[bb:-bb,i],lat[bb:-bb,i])
-    m.plot(x,y,color='red')
+#    x,y = m(lon[bb:-bb,i],lat[bb:-bb,i])
+#    m.plot(x,y,color='red')
 
-    x,y = m(lon[i,bb:-bb],lat[i,bb:-bb])
-    m.plot(x,y,color='red')
+#    x,y = m(lon[i,bb:-bb],lat[i,bb:-bb])
+#    m.plot(x,y,color='red')
 
 #r.close()
 
 
 m.drawcoastlines(linewidth=0.5)
-m.drawcountries(linewidth=0.5)
-m.drawstates(linewidth=0.5)
+m.drawmapboundary(fill_color='#999999')
+#m.drawcountries(linewidth=0.5)
+#m.drawstates(linewidth=0.5)
 #m.drawlsmask()
-m.shadedrelief()
+#m.shadedrelief()
+#m.fillcontinents(color='#E8112D', lake_color='#76a6cc')
+m.fillcontinents(color='#666666', lake_color='#999999')
 
 #plt.title('Figure 3: Grid telescoping into Nunavut\nfor convection permitting simulations', y=-0.12, fontsize=40)
-plt.savefig('domain_grid_color.png', pad_inches=0.0, bbox_inches='tight')
+plt.savefig('domain_center_panartcit.png', pad_inches=0.0, bbox_inches='tight', transparent=True)
